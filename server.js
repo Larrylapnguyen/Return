939f635f18ed741f12933788743c66c6bf9d18fa
Larry Nguyen <larrylapnguyen@gmail.com>
2019-10-31 22:07:02 -0700

    User creation

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
 const app = express();
-
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
+//Connect to DB
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
 
 app.listen(PORT, () => console.log(`Server started on Port${PORT}`));
